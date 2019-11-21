3d471bf4a903c24b3b2f9306d26fd6c9c29a55ff
sreedarsh <sreedarsh@E6220>
2019-11-21 14:43:04 +0530

    Empty Project commit.

---
+++ https:/github.com/SreedarshShajiNSK/SpringMVC.git/Spring4_Spring_MVC/src/main/java/com/mvc/demo/Spring4SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4SpringMvcApplication.class, args);
+	}
+
+}
